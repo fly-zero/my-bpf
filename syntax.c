da08602b3a5a3a6ba70000b0b5d1397618989fee
flyzero <fly.zero@foxmail.com>
2025-07-11 23:58:33 +0800

    feat: 优化 bpf 汇编生成，支持寄存器使用后的释放

--- syntax.c
+++ syntax.c
@@ -67,7 +67,13 @@ static const char *bpf_register_name(int reg) {
     return s_bpf_register_names[reg];  // 返回寄存器名称
 }
 
-static int bpf_register_usage_get_free(struct bpf_register_usage *reg_usage) {
+/**
+ * @brief 分配一个未使用的寄存器
+ *
+ * @param reg_usage 寄存器使用情况
+ * @return int 返回分配的寄存器编号，-1 表示没有可用寄存器
+ */
+static int bpf_register_usage_alloc(struct bpf_register_usage *reg_usage) {
     // 查找第一个未使用的寄存器
     for (int i = 0; i < 64; i++) {
         uint64_t mask = 1ULL << i;
@@ -81,19 +87,40 @@ static int bpf_register_usage_get_free(struct bpf_register_usage *reg_usage) {
 }
 
 /**
+ * @brief 释放一个寄存器的使用标记
+ *
+ * @param reg_usage 寄存器使用情况
+ * @param reg 寄存器编号
+ */
+static void bpf_register_usage_free(struct bpf_register_usage *reg_usage, int reg) {
+    assert(reg >= BPF_REGISTER_R0 && reg <= BPF_REGISTER_R7);
+    uint64_t mask = 1ULL << (reg - BPF_REGISTER_R0);
+    reg_usage->bitmap &= ~mask;  // 清除寄存器使用标记
+}
+
+/**
  * @brief 生成比较操作的 BPF 汇编代码
  *
  * @param reg_usage 寄存器使用情况
- * @param op 比较操作符，例如 "==", "!=", "<", ">", "<=", ">="
- * @param left_reg 左侧寄存器编号
- * @param right_reg 右侧寄存器编号
+ * @param node 结点
+ * @return 成功时返回 0，失败时返回 -1
  */
 static int bpf_node_asm_operator_comparison(struct bpf_register_usage *reg_usage,
-                                            const char                *op,
-                                            int                        left_reg,
-                                            int                        right_reg) {
-    printf("%16s: r%d %s r%d\n", "compare", left_reg, op, right_reg);
-    return BPF_REGISTER_CR;  // 返回比较寄存器编号
+                                            struct bpf_syntax_node    *node) {
+    assert(node->type == BPF_SYNTAX_NODE_OPERATOR_COMPARISON);
+
+    // 输出比较操作的 BPF 汇编代码
+    printf("%16s: %s %s %s\n",
+           "compare",
+           bpf_register_name(node->left->reg),
+           node->str,
+           bpf_register_name(node->right->reg));
+    node->reg = BPF_REGISTER_CR;
+
+    // 释放左右子结点的寄存器
+    bpf_register_usage_free(reg_usage, node->left->reg);
+    bpf_register_usage_free(reg_usage, node->right->reg);
+    return 0;
 }
 
 /**
@@ -115,7 +142,7 @@ static int bpf_argn_to_reg(uint8_t argn) {
  * @brief 生成加载 field 的 BPF 汇编代码
  *
  * @param reg_usage 寄存器使用情况
- * @param field 字段名称
+ * @param node 结点
  * @return 成功时返回 0，失败时返回 -1
  */
 static int bpf_node_asm_field(struct bpf_register_usage *reg_usage, struct bpf_syntax_node *node) {
@@ -124,7 +151,7 @@ static int bpf_node_asm_field(struct bpf_register_usage *reg_usage, struct bpf_s
     struct bpf_syntax_field_node *field = (struct bpf_syntax_field_node *)node;
 
     // 从 reg_usage 找出可用的寄存器
-    int reg = bpf_register_usage_get_free(reg_usage);
+    int reg = bpf_register_usage_alloc(reg_usage);
     if (reg < 0) {
         fprintf(stderr, "No available registers for field: %s\n", node->str);
         return -1;
@@ -132,12 +159,12 @@ static int bpf_node_asm_field(struct bpf_register_usage *reg_usage, struct bpf_s
 
     const struct bpf_syntax_field_attr *attr = field->attr;
     assert(attr);
-    printf("%16s: [%s:%u:%hhu] -> r%d\n",  // [reg:offset:size] -> reg
+    printf("%16s: [%s:%u:%hhu] -> %s\n",  // [reg:offset:size] -> reg
            "load",
            bpf_register_name(bpf_argn_to_reg(attr->argn)),
            attr->offset,
            attr->size,
-           reg);      // 加载字段到寄存器
+           bpf_register_name(reg));      // 加载字段到寄存器
     node->reg = reg;  // 设置结点的寄存器编号
     return 0;
 }
@@ -151,13 +178,13 @@ static int bpf_node_asm_field(struct bpf_register_usage *reg_usage, struct bpf_s
  */
 static int bpf_node_asm_constant(struct bpf_register_usage *reg_usage, const char *constant) {
     // 从 reg_usage 找出可用的寄存器
-    int reg = bpf_register_usage_get_free(reg_usage);
+    int reg = bpf_register_usage_alloc(reg_usage);
     if (reg < 0) {
         fprintf(stderr, "No available registers for constant: %s\n", constant);
         return BPF_REGISTER_INVALID;
     }
 
-    printf("%16s: %s -> r%d\n", "set", constant, reg);
+    printf("%16s: %s -> %s\n", "set", constant, bpf_register_name(reg));
     return reg;  // 返回寄存器编号
 }
 
@@ -174,10 +201,10 @@ static void bpf_node_asm_jump_if(struct bpf_register_usage    *reg_usage,
     assert(node->left && node->left->reg == BPF_REGISTER_CR);
     if (strcmp(node->str, "&&") == 0) {
         printf("%16s: %s\n", "jump_if_true", "+3");  // 向前跳转 3 个指令
-        printf("%16s: 0 -> r0\n", "set");            // 设置 r0 为 0，用于返回结果
+        printf("%16s: 0 -> R0\n", "set");            // 设置 r0 为 0，用于返回结果
     } else if (strcmp(node->str, "||") == 0) {
         printf("%16s: %s\n", "jump_if_false", "+3");  // 向前跳转 3 个指令
-        printf("%16s: 1 -> r0\n", "set");             // 设置 r0 为 0，用于返回结果
+        printf("%16s: 1 -> R0\n", "set");             // 设置 r0 为 0，用于返回结果
     } else {
         fprintf(stderr, "Unknown jump condition: %s\n", node->str);
         return;
@@ -195,7 +222,7 @@ static void bpf_node_asm_jump_if(struct bpf_register_usage    *reg_usage,
 static void bpf_node_asm_right_sub_expression(struct bpf_register_usage    *reg_usage,
                                               const struct bpf_syntax_node *node) {
     assert(node->type == BPF_SYNTAX_NODE_RIGHT_SUB_EXPRESSION);
-    printf("%16s: cs -> r0\n", "mov");
+    printf("%16s: cs -> R0\n", "mov");
 }
 
 static int bpf_node_asm(struct bpf_syntax_node *node, void *arg) {
@@ -203,9 +230,7 @@ static int bpf_node_asm(struct bpf_syntax_node *node, void *arg) {
 
     switch (node->type) {
     case BPF_SYNTAX_NODE_OPERATOR_COMPARISON:
-        node->reg = bpf_node_asm_operator_comparison(
-            reg_usage, node->str, node->left->reg, node->right->reg);
-        return 0;
+        return bpf_node_asm_operator_comparison(reg_usage, node);
     case BPF_SYNTAX_NODE_FIELD:
         return bpf_node_asm_field(reg_usage, node);
     case BPF_SYNTAX_NODE_CONSTANT:
@@ -319,7 +344,7 @@ int bpf_syntax_tree_post_order(struct bpf_syntax_node *node,
                                int (*callback)(struct bpf_syntax_node *, void *),
                                void *arg) {
     if (!node) {
-        return -1;
+        return 0;
     }
 
     if (bpf_syntax_tree_post_order(node->left, callback, arg) < 0) {
